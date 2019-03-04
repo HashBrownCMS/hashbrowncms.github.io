b8f239e10d8e0d0663f993c4a35c46e8ab664b1c
mrzapp <contact@jeppezapp.com>
2019-03-04 14:23:01 +0100

    styling and more docs

--- index.php
+++ index.php
@@ -53,13 +53,90 @@ function path_join() {
     return $path;
 }
 
+function check_doc_link($link) {
+    return
+        $link !== 'index.js' &&
+        $link !== 'server.js' &&
+        $link !== 'client.js' &&
+        $link !== 'common.js' &&
+        $link !== 'demo.js' &&
+        $link !== 'dashboard.js' &&
+        $link !== 'utilities.js' &&
+        $link !== 'helpers.js' &&
+
+        $link !== 'ApiController.js' &&
+        $link !== 'Controller.js' &&
+
+        $link !== 'Schemas' &&
+        $link !== 'Routes' &&
+        $link !== 'Style' &&
+
+        !strpos($link, ' ') &&
+        
+        $link !== 'icons.json';
+}
+
 switch(get_path(0)) {
     case 'docs':
-        include __DIR__ . '/views/docs.php';
-        break;
+        $page_is_class = substr(URI, -3) === '.js';
+        $page_is_view = substr(URI, -4) === '.pug';
+        $page_is_api = strpos(URI, '/API') !== false;
+        $page_is_dir = !$page_is_class && !$page_is_view;
+        
+        $page_title = PATH[sizeof(PATH) - 1];
+        $page_description = '';
 
-    case 'src':
-        include __DIR__ . '/views/src.php';
+        if($page_is_class) {
+            $file_url = 'https://' . path_join('raw.githubusercontent.com/HashBrownCMS/hashbrown-cms/stable', str_replace('docs/', 'src/', URI)); 
+
+            if($page_is_api) {
+                $file_url = str_replace('/API', '/Controllers', $file_url);
+            }
+
+            $file_contents = file_get_contents($file_url);
+
+            $class_name = $page_title;
+
+            $class_description = [];
+            preg_match("/\* ([^@][^\n]+)/", $file_contents, $class_description);
+            $class_description = isset($class_description[1]) ? $class_description[1] : ''; 
+            $page_description = $class_description;
+
+            // Remove the class meta docs to prevent confusion
+            $file_contents = preg_replace("/\/\*\*[^\/]+\//", '', $file_contents, 1);
+
+            $class_member_vars = [];
+            preg_match_all("/this\.def\(([^,]+), '([^']+)'/", $file_contents, $class_member_vars, PREG_SET_ORDER);
+
+            $class_methods = [];
+            preg_match_all("/\/\*\*\n +[^\(]+\([^'\)]*\) {/", $file_contents, $class_methods);
+            
+            include __DIR__ . '/views/docs.php';
+
+        } else if($page_is_view) {
+            $file_url = 'https://' . path_join('raw.githubusercontent.com/HashBrownCMS/hashbrown-cms/stable', str_replace('docs/', 'src/', URI)); 
+            $file_contents = file_get_contents($file_url);
+            
+            include __DIR__ . '/views/docs.php';
+
+        } else if($page_is_dir) {
+            $page_url = 'https://' . path_join('github.com/HashBrownCMS/hashbrown-cms/tree/stable', str_replace('docs/', 'src/', URI));
+            
+            if($page_title === 'docs') {
+                $page_title = 'Documentation';
+            }
+
+            if($page_title === 'API') {
+                $page_url = str_replace('/API', '/Controllers', $page_url);
+            }
+
+            $page_contents = file_get_contents($page_url);
+
+            $page_links = [];
+            preg_match_all('/<a.+class="js-navigation-open" title="([^"]+)"/', $page_contents, $page_links);
+            
+            include __DIR__ . '/views/docs.php';
+        }
         break;
 
     default:
--- views/docs.php
+++ views/docs.php
@@ -1,140 +1,202 @@
-<?php
-    $file_path = path_join(ROOT_DIR, 'docs', 'json');
-
-    foreach(PATH as $namespace) {
-        if($namespace === 'docs' || !$namespace) { continue; }
-    
-        $file_path = path_join($file_path, $namespace);
-    }
-?>
 <!DOCTYPE html>
 <html>
     <?php include __DIR__ . '/inc/head.php'; ?>
 
     <body>
-        <header class="container header">
-            <div class="widget widget--breadcrumbs">
-                <?php 
-                    $breadcrumb_link = '/docs/';
-                    
-                    echo '<a class="widget--breadcrumbs__link" href="' . $breadcrumb_link . '">docs</a>';
-
-                    foreach(PATH as $i => $namespace) {
-                        if($namespace === 'docs' || !$namespace) { continue; }
-
-                        $breadcrumb_link .= $namespace . '/';
-
-                        echo ' / <a class="widget--breadcrumbs__link" ' . ($i < sizeof(PATH) - 1 ? 'href="' . $breadcrumb_link . '"' : '') . '>' . $namespace . '</a>';
-                    }
-                ?>
+        <nav class="menu">
+            <div class="container">
+                <a class="menu__item" href="/getting-started">Getting started</a>
+                <a class="menu__item" href="/guides">Guides</a>
+                <a class="menu__item" href="/"><img class="menu__logo" src="/img/logo_pink.svg"></a>
+                <a class="menu__item" href="/docs">Source docs</a>
+                <a class="menu__item" href="/docs/Server/Controllers">API docs</a>
             </div>
-        </header>
+        </nav>
 
-        <main class="container">
-            <?php if(get_path(2) === 'Controllers') { ?>
-                <div class="widget widget--notice">
-                    <p class="widget--notice__heading">To authorise an API request, you must get an API token, like this:</p>
-                    <pre class="widget widget--code">POST { username: myusername, password: mypassword } /api/user/login?persist=true|false</pre>
-                </div>
+        <header class="header">
+            <div class="container header__container">
+                <?php if($page_title) { ?>
+                    <h1><?php echo $page_title; ?></h1>
+                <?php } ?>
+                
+                <?php if($page_description) { ?>
+                    <p><?php echo $page_description; ?></p>
+                <?php } ?>
+            </div>
+            
+            <?php if(sizeof(PATH) > 1) { ?>
+                <nav class="widget widget--breadcrumbs">
+                    <div class="container">
+                        <?php 
+                            $breadcrumb_link = '/';
+                            
+                            foreach(PATH as $i => $namespace) {
+                                if(!$namespace) { continue; }
+
+                                if($breadcrumb_link !== '/') {
+                                    echo ' / ';
+                                }
+
+                                $breadcrumb_link = path_join($breadcrumb_link, $namespace);
+
+                                if($namespace === 'docs') {
+                                    $namespace = 'Documentation';
+                                }
+                                
+                                if($namespace === 'Controllers') {
+                                    $namespace = 'API';
+                                }
+
+                                echo '<a class="widget--breadcrumbs__link" ' . ($i < sizeof(PATH) - 1 ? 'href="' . $breadcrumb_link . '"' : '') . '>' . $namespace . '</a>';
+                            }
+                        ?>
+                    </div>
+                </nav>
             <?php } ?>
 
-            <?php if(file_exists($file_path . '.json')) { ?>
-                <?php
-                    $entries = file_get_contents($file_path . '.json');
-                    $entries = json_decode($entries);
-                ?>
-
-                <?php foreach($entries as $entry) { ?>
-                    <?php if($entry->kind !== 'class') { continue; } ?>
-            
-                    <div class="class">            
-                        <h1 class="class__name"><?php echo $entry->name; ?></h1>
-
-                        <?php if(isset($entry->classdesc)) { ?>
-                            <h2 class="class__description"><?php echo $entry->classdesc; ?></h2>
-                        <?php } ?>
+            <?php if(get_path(2) === 'Controllers') { ?>
+                <div class="widget widget--notice">
+                    <div class="container">
+                        <p class="widget--notice__heading">To authorise an API request, you must get an API token, like this:</p>
+                        <pre class="code code--uri">POST { username: myusername, password: mypassword } /api/user/login?persist=true|false</pre>
                     </div>
-                <?php } ?>
-
-                <?php foreach($entries as $entry) { ?>
-                    <?php
-                        if(isset($entry->undocumented) || !isset($entry->name) || $entry->name === 'structure' || $entry->kind !== 'function') { continue; }
-
-                        if(isset($entry->returns) && $entry->returns[0]->type->names[0] === 'Promise') {
-                            $entry->async = true;
-                        }
-                    ?>
-
-                    <div class="function">
-                        <h3 class="function__name">
-                            <?php echo $entry->name; ?>
-                        </h3>
-
-                        <ul class="function__meta">                        
-                            <?php if($entry->scope === 'static') { ?>
-                                <li class="function__meta__name">static</li>
-                            <?php } ?>
-                            
-                            <?php if(isset($entry->async) && $entry->async) { ?>
-                                <li class="function__meta__name">async</li>
-                            <?php } ?>
-                        </ul>
-
-                        <?php if(isset($entry->description)) { ?>
-                            <h4 class="function__description"><?php echo $entry->description; ?></h4>
-                        <?php } ?>
-
-                        <?php if(sizeof($entry->params) > 0) { ?>
-                            <h5>Parameters</h5>
-
-                            <ul class="function__params">
-                                <?php foreach($entry->params as $param) { ?>
-                                    <li class="function__param">
-                                        <span class="function__param__type"><?php echo $param->type->names[0]; ?></span>
-                                        <span class="function__param__name"><?php echo $param->name; ?></span>
+                </div>
+            <?php } ?>
+        </header>
         
-                                        <?php if(isset($param->description)) { ?>
-                                            <span class="function__param__description"><?php echo $param->description; ?></span>
-                                        <?php } ?>                
-                                    </li>
-                                <?php } ?>
-                            </ul>
-                        <?php } ?>
-                        
-                        <?php if(isset($entry->returns)) { ?>
-                            <h5>Returns</h5>
+        <main class="container">
+            <?php if($page_is_dir) { ?>
+                <ul class="widget widget--directory">
+                    <?php foreach($page_links[1] as $page_link) { ?>
+                        <?php if(!check_doc_link($page_link)) { continue; } ?>
 
-                            <p>
-                                <span class="function__returns"><?php echo $entry->returns[0]->type->names[0]; ?></span>
-                                <?php echo $entry->returns[0]->description; ?>
-                            </p>
-                        <?php } ?>
-                        
-                        <?php if(isset($entry->examples)) { ?>
-                            <?php foreach($entry->examples as $example) { ?>
-                                <p class="function__example">Example <span class="function__example__code"><?php echo $example; ?></span></p>
-                            <?php } ?>
-                        <?php } ?>
-                    </div>
-                <?php } ?>
+                        <?php
 
-            <?php } else if(file_exists($file_path)) { ?>
-                <ul class="widget widget--list">       
-                    <?php foreach(scandir($file_path) as $class) { ?>
-                        <?php if($class === '.' || $class === '..') { continue; } ?>
+                        $link_text = basename($page_link, '.js');
 
-                        <?php $class = basename($class, '.json'); ?>
+                        if($page_link === 'Controllers') {
+                            $page_link = 'API';
+                        }
 
-                        <li class="widget--list__item">
-                            <a href="<?php echo path_join(URI, $class); ?>"><?php echo $class; ?></a>
+                        if($page_is_api) {
+                            $link_text = str_replace('Controller', '', $link_text);
+                        }
+
+                        ?>
+                            
+                        <li class="widget--directory__item">
+                            <a href="<?php echo path_join(URI, $page_link); ?>"><?php echo $link_text; ?></a>
                         </li>
                     <?php } ?>
                 </ul>
+            
+            <?php } else if($page_is_view) { ?>
+                <h2>Source</h2>
 
-            <?php } else { ?>
-                Not found
+                <pre class="code code--source"><?php echo $file_contents; ?></pre>
 
+            <?php } else if($page_is_class) { ?>
+                <?php if(sizeof($class_member_vars) > 0) { ?>
+                    <h3>Member variables</h3>
+
+                    <ul class="widget widget--parameter-list">
+                        <?php foreach($class_member_vars as $class_member_var) { ?>
+                            <li class="widget--parameter-list__item"><span class="code code--type"><?php echo $class_member_var[1]; ?></span> <?php echo $class_member_var[2]; ?></li>
+                        <?php } ?>
+                    </ul>    
+                <?php } ?>
+                
+                <?php if(sizeof($class_methods) > 0) { ?>
+                    <h2>Functions</h2>
+
+                    <?php foreach($class_methods[0] as $class_method) { ?>
+                        <?php
+                
+                        $class_method_name = [];
+                        preg_match("/([a-zA-Z]+)\(/", $class_method, $class_method_name);
+                        $class_method_name = isset($class_method_name[1]) ? $class_method_name[1] : '';
+
+                        if(!$class_method_name || $class_method_name === 'structure') { continue; }
+
+                        $class_method_description = [];
+                        preg_match("/\* ([^@][^\n]+)/", $class_method, $class_method_description);
+
+                        $class_method_is_static = preg_match("/static/", $class_method);
+                        $class_method_is_async = preg_match("/async/", $class_method);
+                        
+                        $class_method_example = [];
+                        preg_match("/@example ([^\n]+)/", $class_method, $class_method_example);
+
+                        $class_method_params = [];
+                        preg_match_all("/@param.+{([^}]+)} ([^\n]+)/", $class_method, $class_method_params, PREG_SET_ORDER);
+                        
+                        $class_method_returns = [];
+                        preg_match("/@return.+{([^}]+)} ([^\n]+)/", $class_method, $class_method_returns);
+
+                        if(isset($class_method_returns[1]) && $class_method_returns[1] === 'Promise') {
+                            $class_method_is_async = true;
+                        }
+
+                        ?>
+
+                        <section class="section"> 
+                            <h3> 
+                                <?php echo $class_method_name; ?>
+                                <?php if($class_method_is_static) { ?>
+                                    <span class="tag">static</span>
+                                <?php } ?>
+
+                                <?php if($class_method_is_async) { ?>
+                                    <span class="tag">async</span>
+                                <?php } ?>
+                            </h3>
+
+                            <?php if(sizeof($class_method_description) > 0) { ?>
+                                <p><?php echo $class_method_description[1]; ?></p>
+                            <?php } ?>
+                            
+                            <?php if(sizeof($class_method_example) > 0) { ?>
+                                <h4>Example</h4>
+
+                                <p class="code code--uri"><?php echo $class_method_example[1]; ?></p>
+                            <?php } ?>
+
+                            <?php if(sizeof($class_method_params) > 0) { ?>
+                                <h4>Parameters</h4>
+
+                                <ul class="widget widget--parameter-list">
+                                    <?php foreach($class_method_params as $i => $class_method_param) { ?>
+                                        <li class="widget--parameter-list__item">
+                                            <span class="code code--type"><?php echo $class_method_param[1]; ?></span>
+                                            <?php echo $class_method_param[2]; ?>
+                                        </li>
+                                    <?php } ?>
+                                </ul>
+                            <?php } ?>
+                            
+                            <?php if(sizeof($class_method_returns) > 0) { ?>
+                                <h4>Return value</h4>
+
+                                <p>
+                                    <span class="code code--type"><?php echo $class_method_returns[1]; ?></span>
+                                    <?php echo $class_method_returns[2]; ?>
+                                </p>
+                            <?php } ?>
+                        </section>
+                    <?php } ?>
+                <?php } ?>
+
+                <h2>Source</h2>
+
+                <pre class="code code--source"><?php echo $file_contents; ?></pre>
             <?php } ?>
         </main>
+
+        <footer class="footer">
+            <div class="container">
+                HashBrown CMS is licensed under the MIT license<br>
+                Copyright <a href="//putaitu.com">Putaitu</a>
+            </div>
+        </footer>
     </body>
 </html>
