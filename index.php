4ce5ca4c1b4ffb258d2629a3f2f7dc5ee6b25aa4
Jeppe Zapp <contact@jeppezapp.com>
2019-10-11 11:50:14 +0200

    Made serveable

--- index.php
+++ index.php
@@ -2,130 +2,168 @@
 
 require_once('./functions.php');
 
-$html = get_output_cache();
-
-if(!$html) {
-    ob_start();
+if(
+    get_path(0) === 'css' ||
+    get_path(0) === 'js' ||
+    get_path(0) === 'fonts' ||
+    get_path(0) === 'img'
+) {
+    $file_path = ROOT_DIR . URI;
     
-    switch(get_path(0)) {
-        case 'docs':
-            switch(get_path(1)) {
-                case 'guides':
-                    $file_url = WIKI_PAGE_ROOT_URL . str_replace('docs/guides/', '', URI);
-
-                    $GLOBALS['page'] = parse_page($file_url);
-
-                    $title = query_selector_all('//h1')[1]->nodeValue;
-                    $description = '';
-
-                    if($title === 'Home') {
-                        $title = 'Guides';
-                        $description = 'Learn how to get along with HashBrown';
-                    }
-
-                    define('PAGE_TITLE', $title);
-                    define('PAGE_DESCRIPTION', $description);
-                    
-                    include __DIR__ . '/views/docs/guide.php';
-                    break;
-
-                case 'src':
-                    $page = [];
-
-                    if(substr(URI, -1) === '/') {
-                        $page_is_dir = true;
-
-                        $file_url = SRC_DIR_ROOT_URL . str_replace('/docs/src', '/src', URI);
-                        $page = parse_dir($file_url);
-
-                        $page_title = PATH[sizeof(PATH) - 1];
-
-                        if($page_title === 'src') {
-                            $page_title = 'Source docs';
-                        }
-
-                        define('PAGE_TITLE', $page_title);
-                        define('PAGE_DESCRIPTION', '');
-
-                    } else {
-                        $page_is_dir = false;
-
-                        $file_url = SRC_CLASS_ROOT_URL . str_replace('/docs/src', '/src', URI) . '.js';
-                        $page = parse_source_file($file_url);
-                        
-                        define('PAGE_TITLE', $page['name']);
-                        define('PAGE_DESCRIPTION', $page['description']);
-                    }
-                    
-                    include __DIR__ . '/views/docs/src.php';
-                    break;
-
-                case 'api':
-                    $page = [];
-
-                    if(substr(URI, -1) === '/') {
-                        $page_is_dir = true;
-
-                        $file_url = SRC_DIR_ROOT_URL . str_replace('/docs/api', '/src/Server/Controllers', URI);
-                        $page = parse_dir($file_url);
-
-                        $page_title = PATH[sizeof(PATH) - 1];
-                        
-                        if($page_title === 'api') {
-                            $page_title = 'API docs';
-                        }
-
-                        define('PAGE_TITLE', $page_title);
-                        define('PAGE_DESCRIPTION', '');
-
-                    } else {
-                        $page_is_dir = false;
-
-                        $file_url = SRC_CLASS_ROOT_URL . str_replace('/docs/api', '/src/Server/Controllers', URI) . 'Controller.js';
-                        $page = parse_source_file($file_url);
-                        
-                        define('PAGE_TITLE', $page['name']);
-                        define('PAGE_DESCRIPTION', '');
-                    }
-                    
-                    include __DIR__ . '/views/docs/api.php';
-                    break;
-
-                default:
-                    not_found();
-            }
+    switch(strtolower(pathinfo($file_path, PATHINFO_EXTENSION))) {
+        case 'js':
+            $content_type = 'text/javascript';
+            break;
+        
+        case 'css':
+            $content_type = 'text/css';
+            break;
+        
+        case 'svg':
+            $content_type = 'image/svg+xml';
             break;
 
+        case 'ttf':
+            $content_type = 'font/ttf';
+            break;
+    
         default:
-            require_once(__DIR__ . '/lib/hashbrown-driver/index.php');
-
-            HashBrown\init(__DIR__);
-
-            $page = HashBrown\get_current_page();
-
-            if(!$page) { not_found(); }
-
-            if(isset($page->title)) {
-                define('PAGE_TITLE', $page->title);
-            } else {
-                define('PAGE_TITLE', '');
-            }
-
-            if(isset($page->description)) {
-                define('PAGE_DESCRIPTION', $page->description);
-            } else {
-                define('PAGE_DESCRIPTION', '');
-            }
-
-            HashBrown\render_current_page();
+            $content_type = mime_content_type($file_path);
             break;
     }
 
-    $html = ob_get_clean();
-    
-    set_output_cache($html);
+    $output = file_get_contents($file_path);
+
+} else {
+    $content_type = 'text/html';
+
+    $output = get_output_cache();
+
+    if(!$output) {
+        ob_start();
+        
+        switch(get_path(0)) {
+            case 'docs':
+                switch(get_path(1)) {
+                    case 'guides':
+                        $file_url = WIKI_PAGE_ROOT_URL . str_replace('docs/guides/', '', URI);
+
+                        $GLOBALS['page'] = parse_page($file_url);
+
+                        $title = query_selector_all('//h1')[1]->nodeValue;
+                        $description = '';
+
+                        if($title === 'Home') {
+                            $title = 'Guides';
+                            $description = 'Learn how to get along with HashBrown';
+                        }
+
+                        define('PAGE_TITLE', $title);
+                        define('PAGE_DESCRIPTION', $description);
+                        
+                        include __DIR__ . '/views/docs/guide.php';
+                        break;
+
+                    case 'src':
+                        $page = [];
+
+                        if(substr(URI, -1) === '/') {
+                            $page_is_dir = true;
+
+                            $file_url = SRC_DIR_ROOT_URL . str_replace('/docs/src', '/src', URI);
+                            $page = parse_dir($file_url);
+
+                            $page_title = PATH[sizeof(PATH) - 1];
+
+                            if($page_title === 'src') {
+                                $page_title = 'Source docs';
+                            }
+
+                            define('PAGE_TITLE', $page_title);
+                            define('PAGE_DESCRIPTION', '');
+
+                        } else {
+                            $page_is_dir = false;
+
+                            $file_url = SRC_CLASS_ROOT_URL . str_replace('/docs/src', '/src', URI) . '.js';
+                            $page = parse_source_file($file_url);
+                            
+                            define('PAGE_TITLE', $page['name']);
+                            define('PAGE_DESCRIPTION', $page['description']);
+                        }
+                        
+                        include __DIR__ . '/views/docs/src.php';
+                        break;
+
+                    case 'api':
+                        $page = [];
+
+                        if(substr(URI, -1) === '/') {
+                            $page_is_dir = true;
+
+                            $file_url = SRC_DIR_ROOT_URL . str_replace('/docs/api', '/src/Server/Controllers', URI);
+                            $page = parse_dir($file_url);
+
+                            $page_title = PATH[sizeof(PATH) - 1];
+                            
+                            if($page_title === 'api') {
+                                $page_title = 'API docs';
+                            }
+
+                            define('PAGE_TITLE', $page_title);
+                            define('PAGE_DESCRIPTION', '');
+
+                        } else {
+                            $page_is_dir = false;
+
+                            $file_url = SRC_CLASS_ROOT_URL . str_replace('/docs/api', '/src/Server/Controllers', URI) . 'Controller.js';
+                            $page = parse_source_file($file_url);
+                            
+                            define('PAGE_TITLE', $page['name']);
+                            define('PAGE_DESCRIPTION', '');
+                        }
+                        
+                        include __DIR__ . '/views/docs/api.php';
+                        break;
+
+                    default:
+                        not_found();
+                }
+                break;
+
+            default:
+                require_once(__DIR__ . '/lib/hashbrown-driver/index.php');
+
+                HashBrown\init(__DIR__);
+
+                $page = HashBrown\get_current_page();
+
+                if(!$page) { not_found(); }
+
+                if(isset($page->title)) {
+                    define('PAGE_TITLE', $page->title);
+                } else {
+                    define('PAGE_TITLE', '');
+                }
+
+                if(isset($page->description)) {
+                    define('PAGE_DESCRIPTION', $page->description);
+                } else {
+                    define('PAGE_DESCRIPTION', '');
+                }
+
+                HashBrown\render_current_page();
+                break;
+        }
+
+        $output = ob_get_clean();
+        
+        set_output_cache($output);
+    }
 }
 
-echo $html;
+header('Content-Type: ' . $content_type);
+
+echo $output;
 
 ?>
--- views/inc/head.php
+++ views/inc/head.php
@@ -7,7 +7,7 @@
     
     <title><?php echo PAGE_TITLE; ?> | HashBrown CMS</title>
     
-    <link rel="icon" type="image/svg" href="/svg/logo.svg">
+    <link rel="icon" type="image/svg" href="/img/logo.svg">
     <link rel="stylesheet" type="text/css" href="/css/style.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 </head>
